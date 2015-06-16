e30f8cdf7e534e8bb11da87756d77170a75a3784
unknown <545965765@qq.com>
2015-06-16 21:03:05 +0800

    add rootfs & init_rootfs.sh

---
+++ rootfs_shell/init_rootfs.sh
@@ -0,0 +1,34 @@
+cd rootfs;
+pwd;
+mkdir root dev boot etc lib proc sys mnt tmp var home
+chomd 1777 tmp
+mkdir usr/lib usr/modules
+mkdir mnt/etc mnt/jffs2 mnt/yaffs mnt/data mnt/temp
+mkdir var/lib var/lock var/log var/run var/tmp
+chmod 1777 var/tmp
+mknod -m 600 dev/console c 5 1
+mknod -m 600 dev/null c 1 3
+
+#append inittab 
+
+echo "::sysinit:/etc/init.d/rcS
+::askfirst:-/bin/sh
+::respawn:-/bin/sh
+::restart:/sbin/init 
+::ctrlaltdel:/sbin/reboot
+::shutdown:/bin/umount -a -r
+::shutdown:/sbin/swapoff -a
+" > etc/inittab
+
+#create rcS file
+
+mkdir etc/init.d
+echo "#!/bin/sh
+# mount all filesystem defined in "fstab"
+echo "#mount all......."
+/bin/mount -a
+" > etc/init.d/rcS 
+
+
+
+
